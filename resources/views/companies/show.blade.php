d81bf204ed7f453cc4020072dd26cd0b40a20790
andreighervan <andrei.ghervan7@yahoo.com>
2018-03-23 11:09:11 +0200

    Add sidebar to companies view

--- resources/views/companies/show.blade.php
+++ resources/views/companies/show.blade.php
@@ -6,6 +6,7 @@
 
         <!-- The justified navigation menu is meant for single line per list item.
              Multiple lines will require custom code not provided by Bootstrap. -->
+
         <div class="masthead">
             <h3 class="text-muted">Project name</h3>
             <nav>
@@ -19,25 +20,47 @@
                 </ul>
             </nav>
         </div>
-
-        <!-- Jumbotron -->
-        <div class="jumbotron">
-            <h1>{{$company->name}}</h1>
-            <p class="lead">{{$company->description}}</p>
-            <p><a class="btn btn-lg btn-success" href="#" role="button">Get started today</a></p>
-        </div>
-
-        <!-- Example row of columns -->
         <div class="row">
-            @foreach($company->projects as $project)
-            <div class="col-lg-4">
-                <h2>{{$project->name}}</h2>
-                <p class="text-danger">{{$project->description}}</p>
-                <p><a class="btn btn-primary" href="/projects/{{$project->id}}" role="button">View details »</a></p>
-            </div>
-                @endforeach
-        </div>
+            <div class="col-lg-9 col-md-9">
+                <!-- Jumbotron -->
+                <div class="jumbotron">
+                    <h1>{{$company->name}}</h1>
+                    <p class="lead">{{$company->description}}</p>
+                    <p><a class="btn btn-lg btn-success" href="#" role="button">Get started today</a></p>
+                </div>
 
-    </div>
+                <!-- Example row of columns -->
+                <div class="row">
+                    @foreach($company->projects as $project)
+                        <div class="col-lg-4">
+                            <h2>{{$project->name}}</h2>
+                            <p class="text-danger">{{$project->description}}</p>
+                            <p><a class="btn btn-primary" href="/projects/{{$project->id}}" role="button">View details
+                                    Â»</a></p>
+                        </div>
+                    @endforeach
+                </div>
+
+            </div>
+            <div class="col-md-3 col-lg-3 col-sm-3">
+                <div class="sidebar-module">
+                    <h4>Actions</h4>
+                    <ol class="list-unstyled">
+                        <li><a href="/companies/{{$company->id}}/edit">Edit</a></li>
+                        <li><a href="#">Delete</a></li>
+                        <li><a href="#">Add new member</a></li>
+                        
+                    </ol>
+                </div>
+                <div class="sidebar-module">
+                    <h4>Members</h4>
+                    <ol class="list-unstyled">
+                        <li><a href="#">GitHub</a></li>
+                        <li><a href="#">Twitter</a></li>
+                        <li><a href="#">Facebook</a></li>
+                    </ol>
+                </div>
+            </div>
+        </div>
 
 @endsection
